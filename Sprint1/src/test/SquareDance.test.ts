582cb1d5421a0d4f3657f0b88ddca7e8789865f8
Dhruv Dagar <dhruvdagar9868@gmail.com>
2026-09-14 12:40:36 +0100

    fixed the issue

--- Sprint1/src/test/SquareDance.test.ts
+++ Sprint1/src/test/SquareDance.test.ts
@@ -4,7 +4,7 @@ import { Squares } from '../lib/filters/SquareDance';
 describe('Square filter', () => {
   it('square a sequence', () => {
     const input = [4,3,5,16,17,35];
-    const expected = [ 4,4,8,16,25,25];
+    const expected = [ 4,4,8,16,25,36];
     expect(Squares(input)).toEqual(expected);
   });
 });
